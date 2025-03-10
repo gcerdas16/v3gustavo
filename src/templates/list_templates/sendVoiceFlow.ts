9c7e3ed6e1251ca494360bb964064f2dc99d69b0
gcerdas16 <gcerdas16@gmail.com>
2025-03-10 10:15:12 -0600

    3rd commit

--- src/templates/list_templates/sendVoiceFlow.ts
+++
@@ -1,9 +0,0 @@
-import { addKeyword } from "@builderbot/bot";
-
-const sendVoiceFlow = addKeyword("GS0310971").addAction(async (ctx, ctxFn) => {
-  await ctxFn.flowDynamic([
-    { body: "send Audio", media: "./public/assets/audio.mp3" },
-  ]);
-});
-
-export { sendVoiceFlow };
